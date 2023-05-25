d7658132fd74295951afedfb83578399728575c2
ralphscl <ralphlacs308@gmail.com>
2023-05-25 23:52:36 +0800

    Update pokeapi.js

--- src/api/pokeapi.js
+++ src/api/pokeapi.js
@@ -4,12 +4,32 @@
 
 import axios from "axios";
 
-const baseUrl = "https://pokeapi.co/api/v2/"
+export const getPokeApi = async (endpoint) => {
+
+    try {
+        const response = await axios.get(endpoint);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
-export const getPokemon = async (offset, limit) => {
-    
+export const getPokemons = async (offset, limit) => {
+    const baseUrl = "https://pokeapi.co/api/v2/"
     let parameters = `pokemon?offset=${offset}&limit=${limit}`;
+
+    try {
+        const response = await axios.get(baseUrl + parameters);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getPokemonsDEtails = async (idt) => {
+    const baseUrl = "https://pokeapi.co/api/v2/"
+    let parameters = `pokemon/${id}`;
 
     try {
         const response = await axios.get(baseUrl + parameters);
