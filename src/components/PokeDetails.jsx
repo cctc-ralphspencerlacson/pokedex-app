2a86f7051b00c61b59808d8af3f098d4f7696ef8
ralphscl <ralphlacs308@gmail.com>
2023-08-02 00:34:38 +0800

    Display Pokemon Stats in Modal

--- src/components/PokeDetails.jsx
+++ src/components/PokeDetails.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 // Service
 import { getPokemonDescription } from '../service/pokeapi';
+// Util
+import { getStatLabel, getMaxStat } from '../utils/OtherUtils';
 // CSS
 import './PokeDetails.css';
 
 const PokeDetails = ({pokeData, colorScheme}) => {
     const [pokeDesc, setPokeDesc] = useState('');
 
-    
   useEffect(() => {
     fetchDescription();
   }, [pokeData]);
@@ -21,7 +22,7 @@ const PokeDetails = ({pokeData, colorScheme}) => {
       console.error("getPokemonDescription: err: " + error);
     }
   }
-    
+    console.log(pokeData)
   return (
     <>
     <div className={`content bb-${colorScheme}`}>
@@ -39,6 +40,15 @@ const PokeDetails = ({pokeData, colorScheme}) => {
         </div>
         <div className='stat'>
             <h3>STATISTICS</h3>
+            <div>
+              {pokeData.stats.map((item) => {
+                return (
+                  <p>
+                    {getStatLabel(item.stat.name)} ( {item.base_stat} / {getMaxStat(item.stat.name)} ) {item.base_stat}
+                  </p>
+                )
+              })}
+            </div>
         </div>
       </div>
     </div>
---
+++ src/utils/OtherUtils.js
@@ -0,0 +1,26 @@
+export function getStatLabel(label) {
+
+    const tmpltLabel = {
+        'hp': 'HP',
+        'attack': 'ATK',
+        'defense': 'DEF',
+        'special-attack': 'SATK',
+        'special-defense': 'SDEF',
+        'speed': 'SPD'
+    }
+
+    return tmpltLabel[label];
+}
+
+export function getMaxStat(stat) {
+    const tmpltLabel = {
+        'hp': 255,
+        'attack': 165,
+        'defense': 185,
+        'special-attack': 170,
+        'special-defense': 155,
+        'speed': 200
+    }
+
+    return tmpltLabel[stat];
+}
