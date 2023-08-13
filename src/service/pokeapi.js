d4e240fd8161410b65cc73c4f84541f4573a0f6e
ralphscl <ralphlacs308@gmail.com>
2023-08-13 22:09:27 +0800

    Document API services

--- src/service/pokeapi.js
+++ src/service/pokeapi.js
@@ -11,11 +11,21 @@ import axios from "axios";
 
 const baseUrl = "https://pokeapi.co/api/v2/"
 
+/**
+ * Retrieves Pokemon species data by ID.
+ *
+ * @param {number} id - The ID of the Pokemon species.
+ * @returns {Promise<Object>} A Promise that resolves to the data of the Pokemon species.
+ */
 export const getPokemonById = async (id) => {
+    // Construct the API endpoint for the Pokemon species data
     let parameters = `pokemon-species/${id}`;
     
     try {    
+        // Fetch the Pokemon species data by ID
         const response = await axios.get(baseUrl + parameters);
+
+        // Assemble and return the data for the Pokemon species
         const data = {
             count: 1,
             results: [{
@@ -26,10 +36,20 @@ export const getPokemonById = async (id) => {
         
         return data;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves paginated data of Pokemon based on filter criteria.
+ *
+ * @param {string} typeFilter - The type filter.
+ * @param {string} regionFilter - The region filter.
+ * @param {number} offset - The offset value for pagination.
+ * @param {number} limit - The limit value for pagination.
+ * @returns {Promise<Object>} A Promise that resolves to the paginated data of Pokemon.
+ */
 export const getPokemonsPaginated = async (typeFilter, regionFilter, offset, limit) => {
     let parameters = "";
     let data = {};
@@ -40,8 +60,8 @@ export const getPokemonsPaginated = async (typeFilter, regionFilter, offset, lim
             
             const response = await axios.get(baseUrl + parameters);
     
-            let results = [];
-            results = response.data.pokemon.slice(offset, offset + limit);
+            // Paginate the results based on the offset and limit
+            const results = response.data.pokemon.slice(offset, offset + limit);
     
             data = {
                 count: response?.data.pokemon.length,
@@ -53,8 +73,8 @@ export const getPokemonsPaginated = async (typeFilter, regionFilter, offset, lim
             
             const response = await axios.get(baseUrl + parameters);
     
-            let results = [];
-            results = response.data.pokemon_species.slice(offset, offset + limit);
+            // Paginate the results based on the offset and limit
+            const results = response.data.pokemon_species.slice(offset, offset + limit);
     
             data = {
                 count: response?.data.pokemon_species.length,
@@ -73,16 +93,25 @@ export const getPokemonsPaginated = async (typeFilter, regionFilter, offset, lim
     
         return data;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves data of all Pokemon species for search.
+ *
+ * @returns {Promise<Object>} A Promise that resolves to the data of Pokemon species for search.
+ */
 export const getPokemonsSearchData = async () => {
+    // Construct the API endpoint for the Pokemon species data
     let parameters = 'pokemon-species?limit=100000&offset=0';
 
     try {
+        // Fetch the Pokemon species data for search
         const response = await axios.get(baseUrl + parameters);
 
+        // Assemble and return the search data
         const data = {
             count: response?.data.count,
             results: response?.data.results
@@ -90,19 +119,29 @@ export const getPokemonsSearchData = async () => {
 
         return data;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves detailed data of a Pokemon by name.
+ *
+ * @param {string} name - The name of the Pokemon.
+ * @returns {Promise<Object>} A Promise that resolves to the detailed data of the Pokemon.
+ */
 export const getPokemonData = async (name) => {
     try {
+        // Fetch data for the Pokemon species and Pokemon itself
         const pokemonResponse = await axios.get(baseUrl + `pokemon/${name}`);
         const speciesResponse = await axios.get(baseUrl + `pokemon-species/${name}`);
         
         const [pokemonData, speciesData] = await Promise.all([pokemonResponse, speciesResponse]);
 
+        // Fetch Pokemon description asynchronously using the ID from species data
         const pokemonDesc = await getPokemonDescription(speciesData.data.id);
         
+        // Assemble and return the detailed Pokemon data
         const data = {
             id: speciesData.data.id, 
             name: {
@@ -154,27 +193,45 @@ export const getPokemonData = async (name) => {
 
         return data;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves the main region name for a specific Pokemon generation.
+ *
+ * @param {string} generation - The name of the Pokemon generation.
+ * @returns {Promise<string>} A Promise that resolves to the main region name.
+ */
 export const getPokemonRegion = async (generation) => {
     let parameters = `generation/${romanToInteger(extractRomanNumerals(generation))}`;
     
     try {
+        // Fetch data for the specified Pokemon generation
         const response = await axios.get(baseUrl + parameters);
+        // Return the main region name from the fetched data
         return response.data.main_region.name;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves Pokemon types data and transforms it for display.
+ *
+ * @returns {Promise<Object[]>} A Promise that resolves to an array of transformed Pokemon types data.
+ */
 export const getPokemonTypes = async () => {
+    // Construct the API endpoint for the Pokemon types data
     let parameters = `type`;
     
     try {
+        // Fetch the Pokemon types data
         const response = await axios.get(baseUrl + parameters);
 
+        // Filter out the 'unknown' type and transform the data for display
         const filteredData = response.data.results.filter((type) => type.name !== 'unknown');
         const transformedData = filteredData.map(({ name }) => ({
             name: capitalize(name),
@@ -183,16 +240,25 @@ export const getPokemonTypes = async () => {
 
         return transformedData;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves Pokemon generations data and transforms it for display.
+ *
+ * @returns {Promise<Object[]>} A Promise that resolves to an array of transformed Pokemon generations data.
+ */
 export const getPokemonGenerations = async () => {
+    // Construct the API endpoint for the Pokemon generations data
     let parameters = `generation`;
 
     try {
+        // Fetch the Pokemon generations data
         const response = await axios.get(baseUrl + parameters);
 
+        // Transform the data for display
         const transformedData = response.data.results.map(({ name }) => ({
             name: capitalize(formatRomanNumerals(name)),
             value: romanToInteger(extractRomanNumerals(name)),
@@ -200,39 +266,63 @@ export const getPokemonGenerations = async () => {
 
         return transformedData;
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves the description of a Pokemon based on the provided ID.
+ *
+ * @param {number} id - The ID of the Pokemon species.
+ * @returns {Promise<string>} A Promise that resolves to the Pokemon description or 'N/A'.
+ */
 export const getPokemonDescription = async (id) => {
+    // Construct the API endpoint for the Pokemon species data
     let parameters = `pokemon-species/${id}`;
     try {    
+        // Fetch the Pokemon species data
         const response = await axios.get(baseUrl + parameters);
         
+        // Initialize the description variable
         let desc = ""
+        // Find the English flavor text entry for the description
         response.data.flavor_text_entries.forEach(item => {
             if(item.language.name === 'en') {
                 desc = item.flavor_text;
             }
         });
 
+        // Return the description or 'N/A' if not found
         return desc || 'N/A';
     } catch (error) {
+        // Handle errors by logging to the console
         console.error(error);
     }
 }
 
+/**
+ * Retrieves the evolution chain of a Pokemon based on the provided chain URL.
+ *
+ * @param {string} chainUrl - The URL of the evolution chain.
+ * @returns {Promise<Object[]>} A Promise that resolves to an array of Pokemon evolution data.
+ */
 export const getPokemonEvolutionChain = async (chainUrl) => {
     try {
+        // Fetch the evolution chain data from the provided URL
         const response = await axios.get(chainUrl);        
 
         const speciesNames = [];
+        
+        // Recursive function to traverse the evolution chain
         const traverseChain = async (details) => {
             if (details.species) {
 
+                // Fetch data for the species and Pokemon
                 const pokemonResponse = await axios.get(baseUrl + `pokemon/${details.species.name}`);
                 const speciesResponse = await axios.get(baseUrl + `pokemon-species/${details.species.name}`);
     
+                // Collect species information and related data
                 speciesNames.push({
                     name:{
                         en: speciesResponse.data?.name,
@@ -270,12 +360,14 @@ export const getPokemonEvolutionChain = async (chainUrl) => {
             }
 
             if(details.evolves_to.length > 0) {
+                // Recursively traverse the evolution chain
                 traverseChain(details.evolves_to[0]);
             }
 
             return speciesNames;
         }
 
+        // Start traversing the evolution chain from the root
         return traverseChain(response.data.chain);
       } catch (error) {
         console.error('Error:', error.message);
